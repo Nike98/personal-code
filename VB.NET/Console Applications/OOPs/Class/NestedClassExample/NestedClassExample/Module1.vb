f45aae868408d05277bd5ae86649f4991f60befd
Nike98 <nikhilagarwal20@yahoo.com>
2017-11-23 11:19:32 +0530

    Added Nested Class Example

---
+++ VB.NET/Console Applications/OOPs/Class/NestedClassExample/NestedClassExample/Module1.vb
@@ -0,0 +1,48 @@
+﻿'
+' #############################################################################
+' #############################################################################
+'
+'                       Common Example of a Nested Class
+'
+' #############################################################################
+' #############################################################################
+'
+
+Imports System.Console
+
+
+Public Class BaseClass
+
+    Public Sub New()
+
+        WriteLine(" This is the Constructor of the BaseClass")
+
+    End Sub
+
+    Public Class InnerClass
+
+        Public Sub New()
+
+            WriteLine(" This is the Constructor of the InnerClass")
+
+        End Sub
+
+    End Class
+
+End Class
+
+Module Module1
+
+    Sub Main()
+
+        ' Calling the BaseClass through an object
+        Dim obj As New BaseClass
+
+        ' Calling the InnerClass through a new object as it should be called differently
+        Dim objInner As New BaseClass.InnerClass
+
+        ReadLine()
+
+    End Sub
+
+End Module
