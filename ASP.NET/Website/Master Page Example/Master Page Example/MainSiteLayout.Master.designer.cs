59e12d7fcd18882860970e928bc8560bf24382c9
Nike98 <nikhilagarwal20@yahoo.com>
2018-07-25 12:07:44 +0530

    Updated the Master Page Example

--- ASP.NET/Website/Master Page Example/Master Page Example/MainSiteLayout.Master.designer.cs
+++ ASP.NET/Website/Master Page Example/Master Page Example/MainSiteLayout.Master.designer.cs
@@ -31,15 +31,6 @@ namespace Master_Page_Example {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// MainMenu control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Menu MainMenu;
-        
-        /// <summary>
         /// MenuItems control.
         /// </summary>
         /// <remarks>
