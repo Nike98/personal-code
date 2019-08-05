7fa6c7bbc240d727fd582179c528c53c7062a865
Nike98 <nikhilagarwal20@yahoo.com>
2019-08-06 00:52:02 +0530

    added Home and Feedback PHP code in the dash.php file

--- PHP/OnlineExaminationSystem/dash.php
+++ PHP/OnlineExaminationSystem/dash.php
@@ -107,5 +107,114 @@
         </div> <!-- / .container-fluid -->
     </nav>
     <!-- Navigation Menu Code Ends here -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <!-- Home PHP code starts here -->
+            <?php
+            if (@$_GET['q'] == 0){
+                $result = mysqli_query($con, "SELECT * FROM 'quiz' ORDER BY date DESC") or die('Error');
+                echo '<div class="panel">
+                      <div class="table-responsive">
+                      <table class="table table-striped title1">
+                        <tr>
+                            <td>Sr.No.</td>
+                            <td><b>Topic</b></td>
+                            <td><b>Total Questions</b></td>
+                            <td><b>Marks</b></td>
+                            <td><b>Time Limit</b></td>
+                        </tr>';
+                $c = 1;
+                while ($row = mysqli_fetch_array($result)){
+                    $title = $row['title'];
+                    $total = $row['total'];
+                    $sahi = $row['sahi'];
+                    $time = $row['time'];
+                    $eid = $row['eid'];
+                    $q12 = mysqli_query($con, "SELECT score FROM history WHERE eid='$eid' AND email='$email'") or die('Error98');
+                    $rowcount = mysqli_num_rows($q12);
+                    if ($rowcount == 0){
+                        echo'<tr>
+                                <td>'.$c++.'</td>
+                                <td>'.$title.'</td>
+                                <td>'.$total.'</td>
+                                <td>'.$sahi * $total.'</td>
+                                <td>'.$time.'&nbsp;min</td>
+                                <td><b>
+                                    <a href="account.php?q=quiz&step=2&eid="'.$eid.'&n=1&t='.$total.'" class="pull-right btn sub1" style="margin:0px;background:#99cc32">
+                                        <span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>&nbsp;
+                                        <span class="title1"><b>Start</b></span>
+                                    </a>
+                                </b></td>
+                             </tr>';
+                    } else {
+                        echo'<tr>
+                                <td>'.$c++.'</td>
+                                <td>'.$title.'&nbsp;<span title="This Quiz is already solved by you!" class="glyphicon glyphicon-ok" aria-hidden="true"></span></td>
+                                <td>'.$total.'</td>
+                                <td>'.$sahi * $total.'</td>
+                                <td>'.$time.'&nbsp;min</td>
+                                <td><b>
+                                    <a href="update.php?q=quizre&step=25&eid="'.$eid.'&n=1&t='.$total.'" class="pull-right btn sub1" style="margin:0px;background:red">
+                                        <span class="glyphicon glyphicon-repeat" aria-hidden="true"></span>&nbsp;
+                                        <span class="title1"><b>Restart</b></span>
+                                    </a>
+                                </b></td>
+                             </tr>';
+                    }
+                }
+                $c=0;
+                echo'</table></div></div>';
+            }
+            ?>
+            <!-- Home PHP code Ends here -->
+
+            <!-- Feedback PHP code starts here -->
+            <?php
+            if (@$_GET['q'] == 3){
+                $result = mysqli_query($con, "SELECT * FROM 'feedback' ORDER BY 'feedback'.'date' DESC") or die('Error');
+                echo '<div class="panel">
+                      <div class="table-responsive">
+                      <table class="table table-striped title1">
+                        <tr>
+                            <td><b>Sr.No.</b></td>
+                            <td><b>Subject</b></td>
+                            <td><b>Email</b></td>
+                            <td><b>Date</b></td>
+                            <td><b>Time</b></td>
+                            <td><b>By</b></td>
+                            <td></td>
+                        </tr>';
+                $c = 1;
+                while ($row = mysqli_fetch_array($result)){
+                    $date = $row['date'];
+                    $date = date("d-m-y", strtotime($date));
+                    $time = $row['time'];
+                    $subject = $row['subject'];
+                    $name = $rwo['name'];
+                    $email = $row['email'];
+                    $id = $row['id'];
+                    echo'<tr><td>'.$c++.'</td>';
+                    echo'<td><a title="Click to open Feedback" href="dash.php?q=3&fid="'.$id.'">'.$subject.'</a></td>
+                         <td>'.$email.'</td>
+                         <td>'.$date.'</td>
+                         <td>'.$time.'</td>
+                         <td>'.$name.'</td>
+                         <td><a title="Open Feedback" href="dash.php?q=3&fid="'.$id.'>
+                            <b><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></b></a>
+                         </td>';
+                    echo'<td><a title="Delete Feedback" href="update.php?fid="'.$id.'">
+                            <b><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></b></a>
+                         </td>
+                         </tr>';
+                }
+                echo'</table></div></div>';
+            }
+            ?>
+            <!-- Feedback PHP code Ends here -->
+        </div> <!-- / .col-md-12 -->
+    </div> <!-- / .row -->
+</div> <!-- / .container -->
 </body>
 </html>
