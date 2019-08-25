25c3a5f84c6dcb67e355588ae703e50b483a8827
Nike98 <nikhilagarwal20@yahoo.com>
2019-08-26 00:10:00 +0530

    Removed some redundant folders

--- PHP/OnlineExaminationSystem/dbConnection.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $con = mysqli_connect('localhost', 'root', '', 'onlineexamination')
-            or die("Could not connect to MySQL".mysqli_error($con));
-?>
--- PHP/OnlineExaminationSystem/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    sesssion_start();
-    if (isset($_SESSION['email'])){session_destroy();}
-    $ref = @$_GET['q'];
-    header("location:$ref");
-?>
