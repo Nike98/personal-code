1d54e155599f4d510db17915239e158ef1283187
Nike98 <nikhilagarwal20@yahoo.com>
2019-08-26 00:06:05 +0530

    Added perfect number program

---
+++ C/Basic/Perfect Number/main.c
@@ -0,0 +1,32 @@
+///
+/**
+ *      WAP to check if the Number entered by
+ *      User is a Perfect Number or not.
+ */
+///
+
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int num, i, rem, sum = 0, num_copy;
+    printf(" Enter the Number: ");
+    scanf("%d", &num);
+
+    num_copy = num;
+
+    for (i = 1; i < num_copy; i++){
+        rem = num_copy % i;
+        if (rem == 0)
+            sum += i;
+    }
+
+    if (sum == num)
+        printf("\n It is a Perfect Number.");
+    else
+        printf("\n It i Not a Perfect Number.");
+
+    return 0;
+}
