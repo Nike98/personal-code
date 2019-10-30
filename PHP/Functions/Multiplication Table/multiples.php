e3d8bf90a076a99f8bcb908ba67628e9e819d940
Nike98 <nikhilagarwal20@yahoo.com>
2019-10-30 13:04:19 +0530

    modified the Multiplication table file

--- PHP/Functions/Multiplication Table/multiples.php
+++ PHP/Functions/Multiplication Table/multiples.php
@@ -8,24 +8,25 @@
 <body>
 <form action="" method="post">
     <span><b>Enter the Number: </b></span>
-    <span style="margin-left: 50px;"><input type="text" name="num" value="<?php echo $num; ?>"></span><br /><br />
+    <span style="margin-left: 50px;"><input type="text" name="num"></span><br /><br />
     <span><b>Enter the Limit: </b></span>
-    <span style="margin-left: 50px;"><input type="text" name="limit" value="<?php echo $limit; ?>"></span><br /><br />
+    <span style="margin-left: 50px;"><input type="text" name="limit"></span><br /><br />
     <span style="margin-left: 100px;"><input type="submit" name="submit" value="Submit"></span>
     <br /><br /><br />
     <?php
     if (isset($_POST['submit'])){
         $num = $_POST['num'];
+        $limit;
 
-        if (isset($_POST['limit'])){
-            $limit = $_POST['limit'];
-            table($num, $limit);
-        }
+        if (empty($_POST['limit']))
+            $limit = 10;
+
         else
-            table($num);
+            $limit = $_POST['limit'];
+        table($num, $limit);
     }
 
-    function table($num, $limit = 10){
+    function table($num, $limit){
         for ($i = 1; $i <= $limit; $i++)
             echo '&emsp;'.$num.'&nbsp;x&nbsp;'.$i.'&nbsp;=&nbsp; <b>'.$num * $i.'</b><br />';
     }
