8397dca8114fbdd4e9ddbfb08950f6050ec663f7
Nike98 <nikhilagarwal20@yahoo.com>
2020-04-02 21:27:32 +0530

    added stack using Pointers program

---
+++ C++/Data Structures/Stack/Stack Using Pointers/main.cpp
@@ -0,0 +1,71 @@
+///
+/**
+ *      WAP to implement a Stack using Pointers
+ */
+///
+
+
+#include<iostream>
+#include<stdlib.h>
+#include<windows.h>
+
+#define MAX 20
+
+using namespace std;
+
+typedef struct node node;
+
+struct node
+{
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
